635531c7e80ab3590693cc0063f0efb764f0935d
JossKM <jossmooyoung@gmail.com>
2021-02-05 13:41:53 -0500

    Lab 4 complete

--- Weekly/Lab-4/Lab-4.cpp
+++ Weekly/Lab-4/Lab-4.cpp
@@ -1,10 +1,8 @@
-
 ////////////////////////////////////////////////////////////////
-// GAME1011 Lab 3
+// GAME1011 Lab 4
 ////////////////////////////////////////////////////////////////
 
 /////// Topic: Inheritance and Polymorphism
-//"Inheritance" means making a class which
 //https://www.tutorialspoint.com/cplusplus/cpp_inheritance.htm
 //https://www.tutorialspoint.com/cplusplus/cpp_interfaces.htm
 //https://www.tutorialspoint.com/cplusplus/cpp_polymorphism.htm
@@ -27,7 +25,7 @@ virtual destructor
 /////// Lab activities
 
 /////// 1: Setup
-// Clone the repo, set Lab 4 as "Startup Project", or start a new project with a main function and an empty class                                                                                           
+// Clone the repo, set Lab 4 as "Startup Project", or start a new project with a main function                                                                                     
 
 /////// 2: Plan Your Heirarchy
 // Why do you need inheritance?
@@ -70,16 +68,183 @@ virtual destructor
 
 #include <iostream>
 
+
 class GameActor
 {
 private:
 	std::string m_name;
+	int* m_pSomeData;
 
 public:
-	void Update();
+	GameActor(std::string a_name)
+	{
+		m_name = a_name;
+		m_pSomeData = new int[3];
+	}
+
+	// virtual signals that the function can be dynamically linked. 
+	// Meaning it is determined which function to call at runtime (polymorphism!)
+	virtual void Update()
+	{
+		std::cout << GetName() << " (GameActor)" << std::endl;
+	}
+
+	std::string GetName()
+	{
+		return m_name;
+	}
+
+	virtual ~GameActor()
+	{
+		std::cout << "Destructor for GameActor" << std::endl;
+		delete[] m_pSomeData;
+	}
 };
+
+
+// This will be an abstract class... otherwise known as an Interface
+class Drawable
+{
+public:
+	virtual void Draw() = 0; // This is an abstract function. It has no implementation. Also called "Pure virtual"
+};
+
+
+//public inheritance: inherit public members
+//protected inheritance: inherit public and protected
+class GameActorDrawable : public GameActor, public Drawable
+{
+private:
+	//Sprite??
+	//Position??
+	char* m_pRawImageData;
+
+public:
+	GameActorDrawable(std::string a_name) : GameActor(a_name)
+	{
+		// pretend we are loading image data here...
+		m_pRawImageData = new char[10000];
+	}
+
+	void Update() override
+	{
+		std::cout << GetName() << " (Drawable)" << std::endl;
+		//calling base class functionality from a child class
+		GameActor::Update();
+	}
+
+	void Draw() override
+	{
+		std::cout << GetName() << " [(O _ O)]" << std::endl;
+	}
+
+	~GameActorDrawable() override
+	{
+		std::cout << "Destructor for GameActorDrawable" << std::endl;
+		delete[] m_pRawImageData;
+	}
+};
+
+// maybe a class which can be drawn
+// maybe a player class with a different update()
+// maybe a bullet class with a special update
+// What about an enemy class?
 
 int main()
 {
+	const int kNumGameActorsMax = 50000;
+
+
+	GameActor* world[kNumGameActorsMax] = {nullptr};
+
+	world[0] = new GameActor("Luke");
+	world[1] = new GameActor("Darth Vader");
+	world[2] = new GameActorDrawable("Obi-Wan");
+
+	std::cout << "Press any key to instantiate 40,000 GameActorDrawables and place them in our world" << std::endl;
+	getchar();
+
+	//Let's make things crazy by instantiating a LOT of new objects...
+	//Hint: See memory usage with Debug -> Windows -> Show Diagnostic Tools
+	//https://docs.microsoft.com/en-us/visualstudio/profiling/memory-usage?view=vs-2019
+	for (int iDrawable = 0; iDrawable < 40000; iDrawable++)
+	{
+		// Find the first empty slot
+		for (int i = 0; i < kNumGameActorsMax; i++)
+		{
+			// If the slot is empty...
+			GameActor* pActor = world[i];
+			if (pActor == nullptr)
+			{
+				// Add a new GameActorDrawable to it!!!
+				pActor = new GameActorDrawable("C3PO # " + iDrawable);
+				world[i] = pActor;
+			}
+		}
+	}
+
+	std::cout << "Press any key to Update and Draw everything in our world" << std::endl;
+	getchar();
+
+	//Update everything in the world
+	for (int i = 0; i < kNumGameActorsMax; i++)
+	{
+		// make sure we only update items that exist
+		GameActor* pActor = world[i];
+		if (pActor == nullptr)
+		{
+			continue;
+		}
+		
+		// When we call Update() on a GameActor pointer, 
+		// and it dynamically chooses different child class Update behaviour, this is called Polymorphism
+		pActor->Update();
+	}
+
+	//Draw everything
+	for (int i = 0; i < kNumGameActorsMax; i++)
+	{
+		// make sure we only update items that exist
+		GameActor* pActor = world[i];
+
+		// Try to convert to Drawable. This is a dynamic cast. If it fails, it will be nullptr
+		// () is the least specific type of type cast. It will try everything
+		// static_cast is the safest, but doesn't work if you want polymorphism
+		// dynamic_cast works for polymorphism https://en.cppreference.com/w/cpp/language/dynamic_cast
+
+		GameActorDrawable* pDrawable = dynamic_cast<GameActorDrawable*>(pActor);
+
+		// If the cast did not work, or pActor was null to begin with... do not do anything!
+		if (pDrawable == nullptr)
+		{
+			continue;
+		}
+
+		pDrawable->Draw();
+	}
+
+	std::cout << "Press any key to delete everything in our world" << std::endl;
+	getchar();
+
+	//////////////////////////////////////////////////////////////////////
+
+	// Delete everything!
+	for (int i = 0; i < kNumGameActorsMax; i++)
+	{		// make sure we only update items that exist
+		GameActor* pActor = world[i];
+		if (pActor == nullptr)
+		{
+			continue;
+		}
+
+		delete pActor; // Note: this will call the destructor of the class it points to
+		// If the destructor is not marked virtual and we call delete on a GameActor pointer to a GameActorDrawable object,
+		// it will not know to call the destructor in GameActorDrawable, which could cause a memory leak!
+	}
+
+
+	std::cout << "Press any key to say goodbye" << std::endl;
+	getchar();
+
 	return 0;
 }
